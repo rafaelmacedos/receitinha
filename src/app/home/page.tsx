8a34c7b49661c81c4211474001b56bbfff36622a
Ramon Montenegro <ramonmontenegropng@gmail.com>
2023-11-25 17:53:08 -0300

    feat: inserted most popular images and created recently added tabs

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="container relative flex h-screen items-center justify-center bg-zinc-200 bg-opacity-40 lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="h-full w-3/4">
-        <div className="flex h-1/3 w-full items-center justify-center">
+        <div className="flex h-[270px] w-full items-center justify-center">
           <div className="">
             <Image src={receitinhalogo} className="mr-20" alt="Receitinha Logo" />
           </div>
@@ -18,38 +18,58 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="p-7 flex flex-row items-center gap-10 border-red-700">
+        <div>
 
-          <div className="h-[250px] w-[180px] text-center rounded-[100px] bg-white">
-            <img className="p-5 h-[180px] w-[180px]" src="card-receita-1.png" alt="" srcset="" />
-            <span className="text-lg">Café da Manhã</span>
+          <span className="p-10 text-2xl font-bold">Mais Populares</span>
+
+          <div className="p-10 flex items-center gap-10">
+
+            <div className="h-[250px] w-[180px] text-center rounded-[100px] bg-white hover:bg-green-400">
+              <img className="p-5 h-[180px] w-[180px]" src="card-receita-1.png" alt="" srcset="" />
+              <span className="text-lg">Café da Manhã</span>
+            </div>
+
+            <div className="h-[250px] w-[180px] text-center rounded-[100px] bg-white hover:bg-green-400">
+              <img className="p-5 h-[180px] w-[180px]" src="card-receita-2.png" alt="" srcset="" />
+              <span className="text-lg">Lanche Rápido</span>
+            </div>
+
+            <div className="h-[250px] w-[180px] text-center rounded-[100px] bg-white hover:bg-green-400">
+              <img className="p-5 h-[180px] w-[180px]" src="card-receita-3.png" alt="" srcset="" />
+              <span className="text-lg">Crepioca</span>
+            </div>
+
+            <div className="h-[250px] w-[180px] text-center rounded-[100px] bg-white hover:bg-green-400">
+              <img className="p-5 h-[180px] w-[180px]" src="card-receita-4.png" alt="" srcset="" />
+              <span className="text-lg">Cuscuz</span>
+            </div>
+
+            <div className="h-[250px] w-[180px] text-center rounded-[100px] bg-white hover:bg-green-400">
+              <img className="p-5 h-[180px] w-[180px]" src="card-receita-5.png" alt="" srcset="" />
+              <span className="text-lg">Pizza de Pão</span>
+            </div>
+
+            <Button className=" h-50 w-50 rounded-[100%] bg-white text-5xl" >+</Button>
+
           </div>
+          
+        </div>
 
-          <div className="h-[250px] w-[180px] text-center rounded-[100px] bg-white">
-            <img className="p-5 h-[180px] w-[180px]" src="card-receita-1.png" alt="" srcset="" />
-            <span className="text-lg">Café da Manhã</span>
+        <div>
+
+          <span className="p-10 text-2xl font-bold">Enviados Recentemente</span>
+          
+          <div className="p-9 flex items-center gap-10">
+
+            <div className="h-[180px] w-[370px] rounded-[70px] bg-white"></div>
+            <div className="h-[180px] w-[370px] rounded-[70px] bg-white"></div>
+            <div className="h-[180px] w-[370px] rounded-[70px] bg-white"></div>
+
+            <Button className=" h-50 w-50 rounded-[100%] bg-white text-5xl" >+</Button>
           </div>
-
-          <div className="h-[250px] w-[180px] text-center rounded-[100px] bg-white">
-            <img className="p-5 h-[180px] w-[180px]" src="card-receita-1.png" alt="" srcset="" />
-            <span className="text-lg">Café da Manhã</span>
-          </div>
-
-          <div className="h-[250px] w-[180px] text-center rounded-[100px] bg-white">
-            <img className="p-5 h-[180px] w-[180px]" src="card-receita-1.png" alt="" srcset="" />
-            <span className="text-lg">Café da Manhã</span>
-          </div>
-
-          <div className="h-[250px] w-[180px] text-center rounded-[100px] bg-white">
-            <img className="p-5 h-[180px] w-[180px]" src="card-receita-1.png" alt="" srcset="" />
-            <span className="text-lg">Café da Manhã</span>
-          </div>
-
-          <Button className=" h-50 w-50 rounded-[100%] bg-white text-5xl" >+</Button>
 
         </div>
 
-        <div className="h-1/3 w-full">TERCEIRA</div>
       </div>
 
       <div className="relative h-full w-[650px] bg-[url('/hand.png')]">
